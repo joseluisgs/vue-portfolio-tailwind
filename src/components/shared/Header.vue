722192f8c5a00aeb01450108d2a9bf20d7527b2b
José Luis González <joseluisgonsan@gmail.com>
2021-08-21 18:35:56 +0200

    Te ayudo Modal

--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -19,8 +19,8 @@
             <span class="nav__item__logo__text">JoseLuisGS</span>
           </router-link>
         </div>
-
-        <!-- <theme-switcher
+        <!-- Solo lo mostramos en pequeñas pantallas, oculto desde sm -->
+        <theme-switcher
           :theme="theme"
           @themeChanged="updateTheme"
           class="
@@ -36,8 +36,8 @@
             rounded-lg
             ml-10
           "
-        /> -->
-        <!-- Boton Menú cuando estamos en dispositivos pequeños, intercambiamos el botón -->
+        />
+        <!-- Boton Menú cuando estamos en dispositivos pequeños, intercambiamos el botón dependiendo de como esté -->
         <div class="sm:hidden">
           <button @click="isOpen = !isOpen" type="button" class="focus:outline-none">
             <svg
@@ -61,7 +61,7 @@
           </button>
         </div>
       </div>
-      <!-- Menú de navegación. Segúb este abierto la navgeacion em oviles lo ponemos como bloque o no -->
+      <!-- Menú de navegación. Segúb este abierto la navgeacion em moviles lo ponemos como bloque o no -->
       <div
         :class="isOpen ? 'block' : 'hidden'"
         class="items-center justify-center mt-5 ml-3 sm:ml-4 sm:mt-3 sm:flex"
@@ -92,14 +92,136 @@
         </div>
         <!-- Boton de cambio de tema -->
         <div
-          class="px-3 py-3.5 ml-5 font-medium shadow-sm cursor-pointer text-md bg-primary-light dark:bg-ternary-dark rounded-xl"
+          class="
+            px-3
+            py-3.5
+            ml-5
+            font-medium
+            shadow-sm
+            cursor-pointer
+            text-md
+            bg-primary-light
+            dark:bg-ternary-dark
+            rounded-xl
+          "
         >
           <ThemeSwitcher :theme="theme" @theme-changed="updateTheme" />
         </div>
       </div>
     </div>
 
-    <!-- Comienzo del modal de contacto -->
+    <!-- Comienzo del modal de contacto, solo lo mostramos cuando modal es visible -->
+    <transition name="fade">
+      <div v-show="modal" class="fixed inset-0 z-30">
+        <!-- background -->
+        <div
+          v-show="modal"
+          @click="showModal()"
+          class="fixed inset-0 z-20 w-full h-full bg-black bg-opacity-50 bg-filter"
+        ></div>
+        <!--          -->
+        <main class="flex flex-col items-center justify-center w-full h-full">
+          <transition name="fade-up-down">
+            <div v-show="modal" class="z-30 flex items-center">
+              <div
+                class="relative flex-row max-w-md max-h-screen mx-5 shadow-lg xl:max-w-xl lg:max-w-xl md:max-w-xl bg-secondary-light dark:bg-primary-dark rounded-xl"
+              >
+                <div
+                  class="flex justify-between gap-10 p-5 border-b border-ternary-light dark:border-ternary-dark"
+                >
+                  <h5 class="text-2xl text-primary-dark dark:text-primary-light">
+                    ¿Qué tipo de proyecto buscas?
+                  </h5>
+                  <button
+                    class="px-4 font-bold text-primary-dark dark:text-primary-light"
+                    @click="showModal()"
+                  >
+                    X
+                  </button>
+                </div>
+                <div class="w-full h-full p-5 overflow-y-auto modal-body">
+                  <form class="max-w-xl m-4 text-left">
+                    <div class="">
+                      <input
+                        class="modal__input"
+                        id="name"
+                        name="name"
+                        type="text"
+                        required
+                        placeholder="Nombre"
+                        aria-label="Name"
+                      />
+                    </div>
+                    <div class="mt-6">
+                      <input
+                        class="modal__input"
+                        id="email"
+                        name="email"
+                        type="text"
+                        required
+                        placeholder="Email"
+                        aria-label="Email"
+                      />
+                    </div>
+                    <div class="mt-6">
+                      <select
+                        class="modal__input"
+                        id="subject"
+                        name="subject"
+                        required
+                        aria-label="Subject"
+                      >
+                        <option selected disabled>Tipo de proyecto</option>
+                        <option value="web">Aplicacion Web</option>
+                        <option value="mobile">Aplicación Móvil</option>
+                        <option value="teaching">Formación</option>
+                        <option value="evaluation">evaluación de producto</option>
+                      </select>
+                    </div>
+                    <div class="mt-6">
+                      <textarea
+                        class="modal__input"
+                        id="message"
+                        name="message"
+                        cols="14"
+                        rows="6"
+                        placeholder="Descripción del proyecto"
+                      ></textarea>
+                    </div>
+
+                    <div class="mt-6">
+                      <button
+                        class="
+                          px-6
+                          py-2.5
+                          text-white
+                          font-medium
+                          tracking-wider
+                          bg-indigo-500
+                          hover:bg-indigo-600
+                          rounded-lg
+                        "
+                        type="submit"
+                      >
+                        Enviar
+                      </button>
+                    </div>
+                  </form>
+                </div>
+                <div class="px-5 py-3 text-right modal-footer border0-t">
+                  <button
+                    class="px-6 py-2 font-bold bg-blue-500 rounded-lg text-primary-light"
+                    @click="showModal()"
+                  >
+                    Cerrar
+                  </button>
+                </div>
+              </div>
+            </div>
+          </transition>
+        </main>
+      </div>
+    </transition>
     <!-- Final del modal de contacto -->
   </nav>
 </template>
@@ -138,10 +260,14 @@ export default defineComponent({
 
     const showModal = () => {
       if (modal.value) {
-        console.log('modal is open');
+        console.log('Close modal');
+        // Bloqueamos el scroll
+        document.getElementsByTagName('html')[0].classList.remove('overflow-y-hidden');
         modal.value = false;
       } else {
-        console.log('modal is open');
+        console.log('Open modal');
+        // Activamos el scroll
+        document.getElementsByTagName('html')[0].classList.add('overflow-y-hidden');
         modal.value = true;
       }
     };
@@ -152,11 +278,11 @@ export default defineComponent({
       console.log(`theme changed to: ${newTheme}`);
       localStorage.setItem('theme', newTheme);
       if (theme.value === 'light') {
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      document.querySelector('html')!.classList.remove('dark');
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        document.querySelector('html')!.classList.remove('dark');
       } else {
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      document.querySelector('html')!.classList.add('dark');
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        document.querySelector('html')!.classList.add('dark');
       }
     };
 
@@ -169,7 +295,6 @@ export default defineComponent({
     };
   },
 });
-
 </script>
 
 <style scoped>
@@ -180,7 +305,7 @@ export default defineComponent({
 }
 
 .nav__item {
-  @apply  block
+  @apply block
           mb-2
           text-lg
           font-medium
@@ -194,13 +319,27 @@ export default defineComponent({
 }
 
 .nav__item__logo__text {
-  @apply  text-primary-dark
+  @apply text-primary-dark
           dark:text-ternary-light
           hover:text-secondary-dark
           dark:hover:text-secondary-light
           ml-2
           text-lg
           font-semibold;
+}
+
+.modal__input {
+  @apply w-full
+          px-5 py-2
+          border-gray-200
+          rounded-lg
+          border
+          dark:border-secondary-dark
+          text-sm
+          bg-secondary-light
+          dark:bg-ternary-dark
+          text-primary-dark
+          dark:text-ternary-light;
 }
 
 .modal-body {
@@ -242,7 +381,3 @@ export default defineComponent({
   opacity: 0;
 }
 </style>
-
-function changeTheme(value: string) {
-  throw new Error('Function not implemented.');
-}
