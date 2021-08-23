865c4716b6e61fd2a99f3470a23eecb1b40b3ad8
José Luis González <joseluisgonsan@gmail.com>
2021-08-23 12:38:53 +0200

    Init Pinia Store

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 import './assets/css/tailwind.css';
 
 createApp(App)
   .use(router)
+  .use(createPinia())
   .mount('#app');
