46833ae4c9822a0c9411ce5112ac8d8b8c67ecd8
José Luis González <joseluisgonsan@gmail.com>
2021-08-23 11:50:43 +0200

    Single Project

---
+++ src/components/projects/SingleProjectGallery.vue
@@ -0,0 +1,41 @@
+<template>
+  <!-- Project gallery start -->
+  <div class="grid grid-cols-1 mt-12 sm:grid-cols-3 sm:gap-10">
+    <div class="mb-10 sm:mb-0">
+      <img
+        src="../../assets/images/projects/ui-project-1.jpg"
+        class="single-project__image"
+        alt="Project Management UI"
+      />
+    </div>
+    <div class="mb-10 sm:mb-0">
+      <img
+        src="../../assets/images/projects/web-project-2.jpg"
+        class="single-project__image"
+        alt="Web Application Cloud"
+      />
+    </div>
+    <div>
+      <img
+        src="../../assets/images/projects/mobile-project-2.jpg"
+        class="single-project__image"
+        alt="Mobile Application Education"
+      />
+    </div>
+  </div>
+  <!-- Project gallery end -->
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'SingleProjectGallery',
+});
+</script>
+
+<style scoped>
+.single-project__image {
+  @apply shadow-lg cursor-pointer rounded-2xl sm:shadow-none;
+}
+</style>
---
+++ src/components/projects/SingleProjectHeader.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <!-- Project heading and meta info -->
+    <p
+      class="text-3xl font-bold text-left  sm:text-4xl text-primary-dark dark:text-primary-light mt-7 sm:mt-20 mb-7"
+    >
+      Project Management UI
+    </p>
+    <div class="flex">
+      <div class="flex items-center mr-10">
+        <Icon icon="feather:clock" class="single-project__icon" />
+        <span class="single-project__description">23 Ago, 2021</span>
+      </div>
+      <div class="flex items-center">
+        <Icon icon="feather:tag" class="single-project__icon" />
+        <span class="single-project__description">UI / Frontend</span>
+      </div>
+    </div>
+    <!-- Project heading and meta info -->
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { Icon } from '@iconify/vue';
+
+export default defineComponent({
+  name: 'SingleProjectHeader',
+
+  components: {
+    Icon,
+  },
+});
+</script>
+
+<style scoped>
+.single-project__icon {
+  @apply w-4 h-4 text-ternary-dark dark:text-ternary-light;
+}
+.single-project__description {
+  @apply ml-2 leading-none text-primary-dark dark:text-primary-light;
+}
+</style>
