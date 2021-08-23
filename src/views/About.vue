c75ccd1f3c0cee53e56aa308e9bd7a4952311e4e
José Luis González <joseluisgonsan@gmail.com>
2021-08-23 10:39:39 +0200

    Acerca De

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,38 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <div class="container mx-auto">
+      <!-- About me -->
+      <AboutMe />
+      <!-- About me -->
+    </div>
+
+    <!-- About counter -->
+    <AboutCounter />
+    <!-- About counter -->
+
+    <div class="container mx-auto">
+      <!-- About Clients -->
+      <AboutClients />
+      <!-- About Clients -->
+    </div>
   </div>
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import AboutMe from '@/components/about/AboutMe.vue';
+import AboutCounter from '@/components/about/AboutCounter.vue';
+import AboutClients from '@/components/about/AboutClients.vue';
+
+export default defineComponent({
+  name: 'About',
+
+  components: {
+    AboutMe,
+    AboutCounter,
+    AboutClients,
+  },
+});
+</script>
+
+<style scoped>
+</style>
