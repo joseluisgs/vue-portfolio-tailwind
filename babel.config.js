b2e31619a4e9c01d5a9c502a19d8b47a3f631c4d
José Luis González <joseluisgonsan@gmail.com>
2021-08-24 10:36:55 +0200

    Remove Console Logs Production

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
+// Para quitar los console logs
+const plugins = [];
+// remove console outputs in production enviroment!
+if (process.env.VUE_APP_STAGE === 'production') {
+  plugins.push('transform-remove-console');
+}
+
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset',
